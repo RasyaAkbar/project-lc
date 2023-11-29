c40fe6425f8c31dfa8c00817041bee227ca96fe2
RasyaAkbar <125935181+RasyaAkbar@users.noreply.github.com>
2023-11-29 13:37:13 +0700

    Add files via upload
    Searching n (number of) queens position that dont attack each other, using depth first search and backtracking

---
+++ N-Queens.py
@@ -0,0 +1,37 @@
+#N-Queens
+ans = []
+def board_valid(board, row, column):
+    #check column
+    for i in range(row, -1, -1):
+        if board[i][column] == "Q": return False
+    #check left diagonal
+    i, j = row, column
+    while i>=0 and j>=0:
+        if board[i][j] == "Q": return False
+        i-=1
+        j-=1
+    #check right diagonal
+    i, j = row, column
+    while i>=0 and j<len(board):
+        if board[i][j] == "Q": return False
+        i-=1
+        j+=1
+    return True
+
+def dfs(board, row):
+    if row == len(board):
+        ans.append(board[:])
+        return
+    for column in range(len(board)):
+        if board_valid(board, row, column):
+            board[row]=board[row][:column]+ "Q"+ board[row][column+1:]
+            dfs(board, row+1)
+            board[row]=board[row][:column]+ "."+ board[row][column+1:]
+
+def Queens():
+    q = int(input("Input the number of queens "))
+    board = ["."*q]*q
+    dfs(board, 0)
+    return ans
+
+print(Queens())
