ef625c969441467cc6b3d8f6e8177d4db9efb6f6
RasyaAkbar <125935181+RasyaAkbar@users.noreply.github.com>
2023-12-08 14:40:34 +0700

    Update permutation.py
    fixed some bugs

--- permutation.py
+++ permutation.py
@@ -4,17 +4,23 @@ the_set = [0]*size
 for i in range(size):
     the_set[i] = int(input("Input the number of the set: "))
 
-ans = []
-def generatePermutation(start, temp):
-    if len(temp) == len(the_set):
-        ans.append(temp[:])
-        return
-    for i in range(size):
-        if the_set[i] in temp:
-            continue
-        temp.append(the_set[i])
-        generatePermutation(i+1, temp)
-        temp.pop()
+permutation = []
+def generatePermutation():
+        #uses dfs and visited dp
+        visited = [False] * len(the_set)
+        def dfs(temp):
+            if len(temp) == len(the_set) and temp not in permutation:
+                permutation.append(temp[:])
+                return
+            for i in range(len(the_set)):
+                if not visited[i]:
+                    if not(i>0 and not visited[i-1] and the_set[i]==the_set[i-1]):
+                        visited[i] = True
+                        dfs(temp+[the_set[i]])
+                        visited[i] = False
+        dfs([])
+        return permutation
         
-generatePermutation(0, [])
-print(f"Your permutation is: {ans}")
+generatePermutation()
+permutation = sorted(permutation)
+print(f"Your permutation is: {permutation}")
