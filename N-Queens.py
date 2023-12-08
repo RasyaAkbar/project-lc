452b09e093c2dc6fb0a1e52fed40ba745f21cf73
RasyaAkbar <125935181+RasyaAkbar@users.noreply.github.com>
2023-12-08 14:50:44 +0700

    Update N-Queens.py
    Add additional information

--- N-Queens.py
+++ N-Queens.py
@@ -1,5 +1,6 @@
-#N-Queens
+#What is the board of n queens that don't attack each other?
 ans = []
+q = int(input("Input the number of queens "))
 def board_valid(board, row, column):
     #check column
     for i in range(row, -1, -1):
@@ -29,9 +30,12 @@ def dfs(board, row):
             board[row]=board[row][:column]+ "."+ board[row][column+1:]
 
 def Queens():
-    q = int(input("Input the number of queens "))
     board = ["."*q]*q
     dfs(board, 0)
     return ans
 
-print(Queens())
+print("Your queen board is: ")
+for i in range(len(Queens())):
+    print('\n')
+    for j in Queens()[i]:
+        print(j)
